d8c872402c89432911d0f1f2cda360a104eea059
lyni <toPolaris@163.com>
2025-10-08 14:11:54 +0800

    feat(log): pack log item

--- source/casca_log.c
+++ source/casca_log.c
@@ -7,12 +7,19 @@
 #include <stdio.h>
 #include "clog_config.h"
 #include "clog_formatter.h"
+#include "clog_hashmap.h"
 #include "clog_mem_pool.h"
+#include "clog_platform.h"
 #include "clog_secure_func.h"
+#ifdef CLOG_PLATFORM_WINDOWS
+#include <windows.h>
+#elif defined(CLOG_PLATFORM_MACOS) || defined(CLOG_PLATFORM_LINUX) || defined(CLOG_PLATFORM_UNIX)
+#include <pthread.h>
+#endif
 
 static clog_context_t g_context = {0};
 
-static char g_err[CASCA_LOG_ERR_BUF_SIZE] = {0};
+static char g_err[CASCA_LOG_ERR_BUF_SIZE] = {0}; /* NOT thread-safe */
 
 static const clog_setup_f g_setup_funcs[] = {
     clog_formatter_setup,
@@ -198,4 +205,81 @@ void clog_err_append_line(const char* fmt, ...)
 const char* clog_err_get(void)
 {
     return g_err;
+}
+
+static unsigned long long clog_get_thread_id(void)
+{
+#ifdef CLOG_PLATFORM_WINDOWS
+    return GetCurrentThreadId();
+#elif defined(CLOG_PLATFORM_MACOS) || defined(CLOG_PLATFORM_LINUX) || defined(CLOG_PLATFORM_UNIX)
+    return (unsigned long long)pthread_self();
+#else
+    return 0;
+#endif
+}
+
+static void clog_item_init_datetime(clog_item_t* item)
+{
+#ifdef CLOG_PLATFORM_WINDOWS
+    SYSTEMTIME st;
+    GetLocalTime(&st);
+    item->year = st.wYear;
+    item->month = st.wMonth;
+    item->day = st.wDay;
+    item->hour = st.wHour;
+    item->minute = st.wMinute;
+    item->second = st.wSecond;
+    item->millisecond = st.wMilliseconds;
+#elif defined(CLOG_PLATFORM_MACOS) || defined(CLOG_PLATFORM_LINUX) || defined(CLOG_PLATFORM_UNIX)
+    struct timeval tv = {0};
+    gettimeofday(&tv, NULL);
+    struct tm* tm_info = localtime(&tv.tv_sec);
+    item->year = tm_info->tm_year + 1900;
+    item->month = tm_info->tm_mon + 1;
+    item->day = tm_info->tm_mday;
+    item->hour = tm_info->tm_hour;
+    item->minute = tm_info->tm_min;
+    item->second = tm_info->tm_sec;
+    item->millisecond = tv.tv_usec / 1000;
+#else
+    item->year = 1970;
+    item->month = 1;
+    item->day = 1;
+    item->hour = 0;
+    item->minute = 0;
+    item->second = 0;
+    item->millisecond = 0;
+#endif
+}
+
+clog_res_e clog_log(const uint32_t* recorders, const size_t count, const char* module, const char* file,
+                    const char* function, const int line, const clog_level_e level, const char* fmt, ...)
+{
+    char* content = clog_malloc(CASCA_LOG_SINGLE_LOG_MAX_SIZE);
+    CLOG_RET_IF_NULL_X(content, CLOG_NO_MEMORY, "malloc log content buf failed, size = %d",
+                       CASCA_LOG_SINGLE_LOG_MAX_SIZE);
+    clog_item_t item = {.filepath = file,
+                        .filename = file,
+                        .function = function,
+                        .module = module,
+                        .tid = clog_get_thread_id(),
+                        .line = line,
+                        .level = level,
+                        .content = content};
+    va_list args;
+    va_start(args, fmt);
+    CLOG_IGNORE_RES(vsnprintf((char*)item.content, CASCA_LOG_SINGLE_LOG_MAX_SIZE, fmt, args));
+    va_end(args);
+    clog_item_init_datetime(&item);
+    const size_t num = count > CASCA_LOG_TARGET_RECORDER_COUNT ? CASCA_LOG_TARGET_RECORDER_COUNT : count;
+    uint32_t* ptr = (uint32_t*)item.recorders;
+    for (int i = 0; i < CASCA_LOG_TARGET_RECORDER_COUNT; ++i) {
+        if (i < num) {
+            ptr[i] = recorders[i];
+        } else {
+            ptr[i] = CLOG_RECORDER_ID_INVALID;
+        }
+    }
+    /* TODO: pre-filter -> formatter -> post-filter -> dispatcher -> recorder */
+    return CLOG_SUCCESS;
 }
