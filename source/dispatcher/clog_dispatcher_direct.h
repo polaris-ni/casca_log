25942f480b59c6e122e5874d1a711b8a366c5f7c
polaris <toPolaris@163.com>
2025-10-17 19:44:37 +0800

    feat(recorder): add clog_dispatcher_direct support

---
+++ source/dispatcher/clog_dispatcher_direct.h
@@ -0,0 +1,19 @@
+/**
+ * @author polaris
+ * @date  2025/10/17
+ */
+#ifndef CASCA_LOG_CLOG_DISPATCHER_DIRECT_H
+#define CASCA_LOG_CLOG_DISPATCHER_DIRECT_H
+
+#include "clog_dispatcher.h"
+
+#if defined(__cplusplus) || defined(c_plusplus)
+extern "C" {
+#endif
+
+const clog_dispatcher_t* clog_dispatcher_direct(void);
+
+#if defined(__cplusplus) || defined(c_plusplus)
+}
+#endif
+#endif /* CASCA_LOG_CLOG_DISPATCHER_DIRECT_H */
---
+++ source/dispatcher/clog_dispatcher_manager.c
@@ -0,0 +1,22 @@
+/**
+ * @auther polaris
+ * @date  2025/10/15
+ */
+#include "clog_dispatcher_manager.h"
+#include "casca_log.h"
+#include "clog_recorder_manager.h"
+
+clog_res_e clog_dispatch(const uint32_t* recorders, size_t num, const clog_item_t* item)
+{
+    CLOG_RET_IF_NULL_X(recorders, CLOG_INVALID_PARAM, "target recorders is NULL");
+    CLOG_RET_IF_NULL_X(item, CLOG_INVALID_PARAM, "item is NULL");
+    clog_res_e last = CLOG_SUCCESS;
+    for (size_t i = 0; i < num; ++i) {
+        const clog_res_e ret = clog_recoder_write(recorders[i], item);
+        if (ret != CLOG_SUCCESS) {
+            clog_err_append_line("clog_recoder_write log %u to %u failed, ret = %d", item->seq, recorders[i], ret);
+            last = ret;
+        }
+    }
+    return last;
+}
