9311a3e0abf5d0fe40b63999414b248bd8585a05
lyni <toPolaris@163.com>
2025-11-20 00:27:45 +0800

    feature(util): hide EBR struct details

--- source/utils/clog_ebr.h
+++ source/utils/clog_ebr.h
@@ -2,10 +2,8 @@
  * @author Polaris
  * @date  2025/11/19
  */
-#ifndef CASCA_LOG_BUILD_CLOG_EBR_H
-#define CASCA_LOG_BUILD_CLOG_EBR_H
-
-#include <stdatomic.h>
+#ifndef CASCA_LOG_CLOG_EBR_H
+#define CASCA_LOG_CLOG_EBR_H
 
 #include "casca_log_base.h"
 #include "clog_hooks.h"
@@ -16,42 +14,6 @@ extern "C" {
 
 typedef struct clog_ebr_global clog_ebr_global_t;
 typedef struct clog_ebr_thread_local clog_ebr_thread_local_t;
-typedef struct clog_ebr_entry clog_ebr_entry_t;
-
-#define CLOG_EBR_NUM_EPOCHS 3
-
-typedef struct {
-    atomic_uintptr_t head;
-} clog_ebr_retired_list_t;
-
-struct clog_ebr_global {
-    atomic_uint_fast64_t current_epoch;
-    clog_ebr_retired_list_t retired_lists[CLOG_EBR_NUM_EPOCHS];
-    atomic_uintptr_t registered_threads;
-    clog_deallocator_f free;
-};
-
-typedef enum clog_ebr_local_state {
-    CLOG_EBR_LOCAL_ACTIVE = 0,
-    CLOG_EBR_LOCAL_INACTIVE = 1,
-    CLOG_EBR_LOCAL_RELEASED = 2
-} clog_ebr_local_state_e;
-
-struct clog_ebr_thread_local {
-    const clog_ebr_global_t* global;
-    atomic_uint_fast64_t local_epoch;
-    atomic_uint state;
-    atomic_uintptr_t next;
-};
-
-/**
- * @brief 待回收项
- * 代表一个需要延迟释放的内存块或对象。
- */
-struct clog_ebr_entry {
-    void* ptr;
-    atomic_uintptr_t next;
-};
 
 /**
  * create a new global EBR state
@@ -60,12 +22,6 @@ struct clog_ebr_entry {
  * @return #clog_res_e
  */
 clog_res_e clog_ebr_create(clog_ebr_global_t** global, clog_deallocator_f free);
-
-/**
- * destroy a global EBR state, must be called when all threads have performed #clog_ebr_unregister
- * @param global EBR to be destroyed
- */
-void clog_ebr_destroy(clog_ebr_global_t* global);
 
 /**
  * register thread itself to #global
@@ -109,8 +65,13 @@ void clog_ebr_defer_release(clog_ebr_global_t* global, const clog_ebr_thread_loc
  */
 void clog_ebr_poll(clog_ebr_global_t* global);
 
+/**
+ * destroy a global EBR state, must be called when all threads have performed #clog_ebr_unregister
+ * @param global EBR to be destroyed
+ */
+void clog_ebr_destroy(clog_ebr_global_t* global);
 
 #if defined(__cplusplus) || defined(c_plusplus)
 }
 #endif
-#endif /* CASCA_LOG_BUILD_CLOG_EBR_H */
+#endif /* CASCA_LOG_CLOG_EBR_H */
