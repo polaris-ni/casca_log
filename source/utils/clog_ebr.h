52841bcb1e381f61175efc3f6c4becffc209f542
lyni <toPolaris@163.com>
2025-11-21 19:09:29 +0800

    feature(util): EBR detail improvements

--- source/utils/clog_ebr.h
+++ source/utils/clog_ebr.h
@@ -12,6 +12,19 @@
 extern "C" {
 #endif
 
+typedef enum clog_ebr_local_state {
+    CLOG_EBR_LOCAL_STATE_UNKNOWN = 0,
+    CLOG_EBR_LOCAL_STATE_ACTIVE = 1,
+    CLOG_EBR_LOCAL_STATE_INACTIVE = 2,
+    CLOG_EBR_LOCAL_STATE_RELEASED = 3
+} clog_ebr_local_state_e;
+
+typedef enum clog_ebr_global_state {
+    CLOG_EBR_GLOBAL_STATE_UNKNOWN = 0,
+    CLOG_EBR_GLOBAL_STATE_ACTIVE = 1,
+    CLOG_EBR_GLOBAL_STATE_INACTIVE = 2,
+} clog_ebr_global_state_e;
+
 typedef struct clog_ebr_global clog_ebr_global_t;
 typedef struct clog_ebr_thread_local clog_ebr_thread_local_t;
 
@@ -24,6 +37,20 @@ typedef struct clog_ebr_thread_local clog_ebr_thread_local_t;
 clog_res_e clog_ebr_create(clog_ebr_global_t** global, clog_deallocator_f free);
 
 /**
+ * get global state
+ * @param global global EBR
+ * @return #clog_ebr_global_state_t
+ */
+clog_ebr_global_state_e clog_ebr_get_global_state(const clog_ebr_global_t* global);
+
+/**
+ * get thread local state
+ * @param local thread local EBR
+ * @return #clog_ebr_local_state_e
+ */
+clog_ebr_local_state_e clog_ebr_get_local_state(const clog_ebr_thread_local_t* local);
+
+/**
  * register thread itself to #global
  * @param global an initialized global EBR
  * @param local thread local EBR
@@ -32,17 +59,20 @@ clog_res_e clog_ebr_create(clog_ebr_global_t** global, clog_deallocator_f free);
 clog_res_e clog_ebr_register(clog_ebr_global_t* global, clog_ebr_thread_local_t** local);
 
 /**
- * register thread in global
+ * register thread in global, it must be called after the thread leave the critical section
  * @param local thread local EBR
  */
 void clog_ebr_unregister(clog_ebr_thread_local_t* local);
 
 /**
  * thread entry critical section
- * @param global global EBR
  * @param local thread local EBR
+ * @return #CLOG_INVALID_PARAM if local or global is NULL
+ * #CLOG_ABNORMAL_STATE if global state is not CLOG_EBR_GLOBAL_STATE_ACTIVE, you should call #clog_ebr_unregister
+ * #CLOG_ALREADY_EXISTED if local state is CLOG_EBR_LOCAL_STATE_ACTIVE(NOTE: local epoch will be updated)
+ * #CLOG_SUCCESS otherwise
  */
-void clog_ebr_enter(clog_ebr_global_t* global, clog_ebr_thread_local_t* local);
+clog_res_e clog_ebr_enter(clog_ebr_thread_local_t* local);
 
 /**
  * thread exit critical section
@@ -52,11 +82,10 @@ void clog_ebr_exit(clog_ebr_thread_local_t* local);
 
 /**
  * deferred memory release
- * @param global global EBR
  * @param local thread local EBR
  * @param ptr memory to be released
  */
-void clog_ebr_defer_release(clog_ebr_global_t* global, const clog_ebr_thread_local_t* local, void* ptr);
+void clog_ebr_defer_release(const clog_ebr_thread_local_t* local, void* ptr);
 
 /**
  * release memory by checking the status of all registered threads
@@ -66,10 +95,13 @@ void clog_ebr_defer_release(clog_ebr_global_t* global, const clog_ebr_thread_loc
 void clog_ebr_poll(clog_ebr_global_t* global);
 
 /**
- * destroy a global EBR state, must be called when all threads have performed #clog_ebr_unregister
+ * destroy a global EBR state, must be called when all threads have performed #clog_ebr_unregister.
+ * If there are threads that have not been unregistered, the actual destroy operation will be delayed
+ * until all thread cancellation operations are completed and CLOG_NOT_COMPLETED will be returned
  * @param global EBR to be destroyed
+ * @return CLOG_NOT_COMPLETED if there exists an unregistered thread, CLOG_SUCCESS otherwise
  */
-void clog_ebr_destroy(clog_ebr_global_t* global);
+clog_res_e clog_ebr_destroy(clog_ebr_global_t* global);
 
 #if defined(__cplusplus) || defined(c_plusplus)
 }
