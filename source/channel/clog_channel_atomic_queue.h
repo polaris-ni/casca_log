c4bae8526622b19211199972a2be754d31f57ff1
lyni <toPolaris@163.com>
2025-12-04 23:46:48 +0800

    feature(channel): support atomic queue channel

---
+++ source/channel/clog_channel_atomic_queue.h
@@ -0,0 +1,24 @@
+/**
+ * @author Polaris
+ * @date  2025/12/4
+ */
+#ifndef CASCA_LOG_CLOG_CHANNEL_ATOMIC_QUEUE_H
+#define CASCA_LOG_CLOG_CHANNEL_ATOMIC_QUEUE_H
+
+#include "clog_channel_base.h"
+
+#if defined(__cplusplus) || defined(c_plusplus)
+extern "C" {
+#endif
+
+/**
+ * get atomic queue channel
+ * @param channel channel
+ * @return #clog_res_e
+ */
+clog_res_e clog_get_atomic_queue_channel(clog_channel_t* channel);
+
+#if defined(__cplusplus) || defined(c_plusplus)
+}
+#endif
+#endif /* CASCA_LOG_CLOG_CHANNEL_ATOMIC_QUEUE_H */
