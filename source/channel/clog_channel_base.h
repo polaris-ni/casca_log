17bac93e429103e0d0ca00a57c7937900caf819b
lyni <toPolaris@163.com>
2025-12-04 23:46:48 +0800

    feature(channel): support atomic queue channel

---
+++ source/channel/clog_channel_base.h
@@ -0,0 +1,39 @@
+/**
+ * @author Polaris
+ * @date  2025/12/4
+ */
+#ifndef CASCA_LOG_CLOG_CHANNEL_BASE_H
+#define CASCA_LOG_CLOG_CHANNEL_BASE_H
+
+#include "casca_log_base.h"
+#include "clog_config.h"
+
+#if defined(__cplusplus) || defined(c_plusplus)
+extern "C" {
+#endif
+
+#define CLOG_CHANNEL_ID_INVALID 0
+#define CLOG_CHANNEL_ID_ATOMIC_QUEUE 1
+
+typedef struct clog_channel clog_channel_t;
+
+typedef uint32_t clog_channel_id_t;
+
+typedef clog_res_e (*clog_channel_open_f)(clog_channel_t* self, const clog_config_group_t* config);
+typedef clog_res_e (*clog_channel_write_f)(clog_channel_t* self, const clog_item_t* item);
+typedef clog_res_e (*clog_channel_read_f)(clog_channel_t* self, const clog_item_t** item);
+typedef void (*clog_channel_close_f)(clog_channel_t* self);
+
+struct clog_channel {
+    clog_channel_id_t id;
+    clog_channel_open_f open;
+    clog_channel_write_f write;
+    clog_channel_read_f read;
+    clog_channel_close_f close;
+    void* param;
+};
+
+#if defined(__cplusplus) || defined(c_plusplus)
+}
+#endif
+#endif /* CASCA_LOG_CLOG_CHANNEL_BASE_H */
