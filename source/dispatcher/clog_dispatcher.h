8bff3f46291a74574565b17604784ca8a87044fe
lyni <toPolaris@163.com>
2026-02-07 10:27:41 +0800

    feature(channel): temporary codes

--- source/dispatcher/clog_dispatcher.h
+++ source/dispatcher/clog_dispatcher.h
@@ -5,16 +5,15 @@
 #ifndef CASCA_LOG_CLOG_DISPATCHER_H
 #define CASCA_LOG_CLOG_DISPATCHER_H
 
-#include "clog_config.h"
+#include "clog_channel_base.h"
 
 #if defined(__cplusplus) || defined(c_plusplus)
 extern "C" {
 #endif
 
 #define CLOG_DISPATCHER_ID_INVALID 0U
-#define CLOG_DISPATCHER_ID_DIRECT 1U
-#define CLOG_DISPATCHER_ID_ASYNC_THREAD 2U
-#define CLOG_DISPATCHER_ID_RESERVED 1000U
+#define CLOG_DISPATCHER_ID_ASYNC_THREAD 1U
+#define CLOG_DISPATCHER_ID_RESERVED 0xFFFFU
 
 typedef struct clog_dispatcher clog_dispatcher_t;
 
@@ -27,17 +26,6 @@ typedef struct clog_dispatcher clog_dispatcher_t;
 typedef clog_res_e (*clog_dispatcher_open_f)(clog_dispatcher_t* self, const clog_config_group_t* group);
 
 /**
- * dispatcher log item to target recorders
- * @param self clog_dispatcher_t itself
- * @param recorders target recorders
- * @param num the num of target recorders
- * @param item log
- * @return clog_res_e
- */
-typedef clog_res_e (*clog_dispatcher_dispatch_f)(clog_dispatcher_t* self, const uint32_t* recorders, size_t num,
-                                                 const clog_item_t* item);
-
-/**
  * close dispatcher, resources should be cleanup at this function
  * @param self clog_dispatcher_t itself
  * @return clog_res_e
@@ -45,36 +33,14 @@ typedef clog_res_e (*clog_dispatcher_dispatch_f)(clog_dispatcher_t* self, const 
 typedef void (*clog_dispatcher_close_f)(clog_dispatcher_t* self);
 
 struct clog_dispatcher {
-    uint32_t id; /* dispatcher id, 0 ~ 1000 is reserved */
+    uint32_t id;
+    clog_channel_t* channel;
     clog_dispatcher_open_f open;
-    clog_dispatcher_dispatch_f dispatch;
     clog_dispatcher_close_f close;
     void* extra;
 };
 
 typedef void (*clog_dispatcher_provider_f)(clog_dispatcher_t* dispatcher);
-
-static clog_res_e clog_dispatcher_empty_open(clog_dispatcher_t* self, const clog_config_group_t* group)
-{
-    CLOG_UNUSED_VAR(self);
-    CLOG_UNUSED_VAR(group);
-    return CLOG_SUCCESS;
-}
-
-static clog_res_e clog_dispatcher_empty_dispatch(clog_dispatcher_t* self, const uint32_t* recorders, size_t num,
-                                                 const clog_item_t* item)
-{
-    CLOG_UNUSED_VAR(self);
-    CLOG_UNUSED_VAR(recorders);
-    CLOG_UNUSED_VAR(num);
-    CLOG_UNUSED_VAR(item);
-    return CLOG_SUCCESS;
-}
-
-static void clog_dispatcher_empty_close(clog_dispatcher_t* self)
-{
-    CLOG_UNUSED_VAR(self);
-}
 
 #if defined(__cplusplus) || defined(c_plusplus)
 }
