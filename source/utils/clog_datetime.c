5a7ff4552312a347907055299489b2bd5b53385f
lyni <toPolaris@163.com>
2026-02-23 18:22:05 +0800

    feature(utils): support datetime api

---
+++ source/utils/clog_datetime.c
@@ -0,0 +1,41 @@
+/**
+ * @author Polaris
+ * @date 2026/2/23
+ */
+
+#include "clog_datetime.h"
+
+#include "casca_log_base.h"
+#ifdef CLOG_PLATFORM_WINDOWS
+#include <Windows.h>
+#else
+#include <time.h>
+#include <sys/time.h>
+#endif
+
+void clog_datetime_now(clog_datetime_t *datetime) {
+    CLOG_RET_VOID_IF_NULL(datetime);
+#ifdef CLOG_PLATFORM_WINDOWS
+        SYSTEMTIME st;
+        GetLocalTime(&st);
+        datetime->year = st.wYear;
+        datetime->month = (uint8_t)st.wMonth;
+        datetime->day = (uint8_t)st.wDay;
+        datetime->hour = (uint8_t)st.wHour;
+        datetime->minute = (uint8_t)st.wMinute;
+        datetime->second = (uint8_t)st.wSecond;
+        datetime->millisecond = st.wMilliseconds;
+#else
+        struct timeval tv;
+        CLOG_IGNORE_RES(gettimeofday(&tv, NULL));
+        struct tm tm_info;
+        CLOG_IGNORE_RES(localtime_r(&tv.tv_sec, &tm_info));
+        datetime->year = tm_info.tm_year + 1900;
+        datetime->month = tm_info.tm_mon + 1;
+        datetime->day = tm_info.tm_mday;
+        datetime->hour = tm_info.tm_hour;
+        datetime->minute = tm_info.tm_min;
+        datetime->second = tm_info.tm_sec;
+        datetime->millisecond = tv.tv_usec / 1000;
+#endif
+}
